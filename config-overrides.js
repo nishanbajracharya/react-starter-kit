7bad731ae45efb8c0ee65c460f6041ec8c4a94cc
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-02-11 17:39:20 +0545

    Add env variables config

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+const env = dotenv.config();
+
+const envVariables = env.error ?  {} : Object.keys(env.parsed)
+  .reduce((acc, key) => ({
+    ...acc,
+    [key]: JSON.stringify(process.env[key])
+  }), {});
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+
+  const envPlugin = new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      ...envVariables
+    }
+  })
+
+  config.plugins.push(envPlugin)
+
+  return config;
+}
