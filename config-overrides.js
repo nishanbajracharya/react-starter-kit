9d62fee81f7806a548d37fa5ad512a515379585e
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2019-02-13 14:39:03 +0545

    Add webpack namespace in config file

--- config-overrides.js
+++ config-overrides.js
@@ -6,41 +6,47 @@ const argv = require('minimist')(process.argv.slice(2));
 function getEnvironmentFile(env) {
   switch (env) {
     case 'example':
-      return path.resolve('./.env.example')
+      return path.resolve('./.env.example');
     default:
       return path.resolve('./.env');
   }
 }
 
-module.exports = function override(config, environment) {
-  // Inject environment using shell arguments or in package.json.
-  // This does not override NODE_ENV and is only limited to this file.
-  // Use this to choose env files for different environments
-  // Eg: $ yarn start --environment=production
-  const injectedEnvironment = argv.environment || environment;
-  console.log('Enviromnent:', injectedEnvironment);
+module.exports = {
+  webpack: function override(config, environment) {
+    // Inject environment using shell arguments or in package.json.
+    // This does not override NODE_ENV and is only limited to this file.
+    // Use this to choose env files for different environments
+    // Eg: $ yarn start --environment=production
+    const injectedEnvironment = argv.environment || environment;
+    console.log('Enviromnent:', injectedEnvironment);
 
-  const envPath = getEnvironmentFile(injectedEnvironment);
-  console.log('Env file:', envPath);
+    const envPath = getEnvironmentFile(injectedEnvironment);
+    console.log('Env file:', envPath);
 
-  const env = dotenv.config({
-    path: envPath
-  });
+    const env = dotenv.config({
+      path: envPath
+    });
 
-  const envVariables = env.error ?  {} : Object.keys(env.parsed)
-    .reduce((acc, key) => ({
-      ...acc,
-      [key]: JSON.stringify(env.parsed[key])
-    }), {});
+    const envVariables = env.error
+      ? {}
+      : Object.keys(env.parsed).reduce(
+          (acc, key) => ({
+            ...acc,
+            [key]: JSON.stringify(env.parsed[key])
+          }),
+          {}
+        );
 
-  const envPlugin = new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      ...envVariables
-    }
-  })
+    const envPlugin = new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        ...envVariables
+      }
+    });
 
-  config.plugins.push(envPlugin);
+    config.plugins.push(envPlugin);
 
-  return config;
-}
+    return config;
+  }
+};
