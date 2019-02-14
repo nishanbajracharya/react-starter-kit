500feb68e549c506d52b6f348e185d755f9d0ed8
prabhtz <gunpaul41@gmail.com>
2019-02-14 14:58:24 +0545

    Use router in App component

--- src/components/AuthRoute.js
+++ src/components/AuthRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router';
 
-import * as routes from './constants/routes';
+import * as routes from '../constants/routes';
 
 /**
  * Component that checks for accessToken and renders auth routes.
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router';
 
-import * as routes from './constants/routes';
+import * as routes from '../constants/routes';
 
 const PrivateRoute = (props) => {
   const { accessToken } = props;
